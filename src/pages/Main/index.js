5cbbc2e5b5858702c712bd964a3344f091397804
MarquisAlexander <marquissantos123@gmail.com>
2019-12-23 17:39:48 -0300

    atualização

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import { Container, Content, Card, CardContent, CardFooter, Title, Description, Annotation } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardSaldo, Title, Description, Annotation } from './styles';
 
-export default function Main() {
-  return (
+export default class Main extends Component {
+  render() {
+    return (
     <Container>
       <Header />
-
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="Visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 7.189,87</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Tranferencia de R$ 20,00 recebida de Fernando hoje as 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu />
+        
+      <Card>
+      <CardHeader>
+        <Icon name="attach-money" size={28} color="#666" />
+        <Icon name="visibility-off" size={28} color="#666" />
+      </CardHeader>
+      <CardContent>
+        <Title>Saldo disponível</Title>
+        <Description>R$ 197.611,65</Description>
+      </CardContent>
+      <CardSaldo>
+        <Annotation>
+          Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 06:00h
+        </Annotation>
+      </CardSaldo>
+      </Card>
       </Content>
-
       <Tabs />
-    </Container>
+      </Container>
   );
 }
+}
+
